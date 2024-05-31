9e79d49c9da9312ff202c98000f0ea020daefecc
Taskeenhaider509 <taskeen_haider_intern@netixsol.com>
2024-05-31 14:50:04 +0500

    Version-1

---
+++ Frontend/src/components/TodoList.js
@@ -0,0 +1,81 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import CreateTask from '../modals/CreateTask';
+import Card from './Cards';
+import axios from 'axios';
+const TodoList = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+    const [stateManage, setStateManage] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        handleGetALLTodo()
+    }, [stateManage])
+
+    const handleGetALLTodo = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.get("https://day-6-backend.vercel.app/get-all-notes");
+            const data = response.data;
+            console.log(data.data);
+            setLoading(false)
+            setTaskList(data.data);
+
+        } catch (error) {
+            console.error("Error occurred:", error);
+        }
+    };
+
+    const deleteTask = async (id) => {
+        try {
+            const response = await fetch(`https://day-6-backend.vercel.app/delete-notes/${id}`, {
+                method: "delete"
+            });
+
+            const data = await response.json();
+            console.log(data.message);
+            setStateManage(prev => !prev)
+
+        } catch (error) {
+            console.error("Error occurred:", error);
+        }
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
+
+    return (
+        <>
+            <div className="header text-center">
+                <h3>Notes App</h3>
+                <button className="btn btn-primary mt-2" onClick={() => setModal(true)} >Create Task</button>
+            </div>
+            <div className="task-container" >
+                {loading ? <h2>
+                    "Loading..."
+                </h2> :
+                    taskList && taskList.map((obj, index) => <Card key={index + "card-task-main"} stateManage={stateManage} setStateManage={setStateManage} taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
+            </div>
+            <CreateTask toggle={toggle} modal={modal} save={saveTask} stateManage={stateManage} setStateManage={setStateManage} />
+        </>
+    );
+};
+
+export default TodoList;
